f819d6421ae33cb18326ba0a3babc2909020af76
tommills27 <tmillsdeveloper@gmail.com>
2016-03-03 01:29:20 +0000

    Completed Location Class

--- src/Location.java
+++ src/Location.java
@@ -13,27 +13,19 @@ public class Location {
 
     private float xcoord;
     private float ycoord;    
-    //e.g. could be GPS coords, or could be metres relative to the size of the farm  
     private String type;
-    public Farm farm;
-    public FieldStation fieldStation;
-    public Sensor sensor;
-    public Data data;
 
     public float getXCoord() {
-        // TODO implement here
-        return 0.0f;
+        return xcoord;
     }
 
 
     public float getYCoord() {
-        // TODO implement here
-        return 0.0f;
+        return ycoord;
     }
 
     public String getType() {
-        // TODO implement here
-        return "";
+        return type;
     }
 
 }
